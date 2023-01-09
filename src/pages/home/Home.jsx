70dd8d5c3a94270476b251b627524ed11183c679
belaymit <2belamit@gmail.com>
2023-01-09 11:01:10 +0300

    imported navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import './home.scss';
 
@@ -6,7 +7,7 @@ const Home = () => (
   <div className="home">
     <Sidebar />
     <div className="home-container">
-      home
+      <Navbar />
     </div>
   </div>
 );
