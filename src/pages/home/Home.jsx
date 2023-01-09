a19e7bef7c7794a1437a56728f1458339177aabd
belaymit <2belamit@gmail.com>
2023-01-09 08:36:46 +0300

    added home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from '../../components/sidebar/Sidebar';
+import './home.scss';
+
+const Home = () => (
+  <div className="home">
+    <Sidebar />
+    <div className="home-container">
+      home
+    </div>
+  </div>
+);
+
+export default Home;
