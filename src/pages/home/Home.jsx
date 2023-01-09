6f67ae4e5cb3518743c5704cde17139bd5decbb0
belaymit <2belamit@gmail.com>
2023-01-09 14:49:15 +0300

    added all reusable components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
+import Widget from '../../components/widgets/Widget';
 import './home.scss';
 
 const Home = () => (
@@ -8,6 +9,12 @@ const Home = () => (
     <Sidebar />
     <div className="home-container">
       <Navbar />
+      <div className="widgets">
+        <Widget type="user" />
+        <Widget type="order" />
+        <Widget type="earnings" />
+        <Widget type="balance" />
+      </div>
     </div>
   </div>
 );
