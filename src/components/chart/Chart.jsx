ae7764572b8869dcb346937da700991a69168134
belaymit <2belamit@gmail.com>
2023-02-03 13:27:43 +0300

    reduced chart height

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,8 +1,66 @@
 import React from 'react';
+import {
+  AreaChart, Area, XAxis, CartesianGrid, Tooltip, ResponsiveContainer,
+} from 'recharts';
 import '../../styles/chart.scss';
 
+const data = [
+  {
+    name: 'January',
+    total: 1200,
+  },
+  {
+    name: 'February',
+    total: 2100,
+  },
+  {
+    name: 'March',
+    total: 800,
+  },
+  {
+    name: 'April',
+    total: 1600,
+  },
+  {
+    name: 'May',
+    total: 900,
+  },
+  {
+    name: 'June',
+    total: 1700,
+  },
+];
+
 const Chart = () => (
-  <div className="chart container">Chart</div>
+  <div className="chart container">
+    <div className="title">Last six Month&apos;s (Revenue)</div>
+    <ResponsiveContainer width="100%" aspect={3 / 1}>
+      <AreaChart
+        width={730}
+        height={250}
+        data={data}
+        margin={{
+          top: 10, right: 30, left: 0, bottom: 0,
+        }}
+      >
+        <defs>
+          <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="name" stroke="gray" />
+        <CartesianGrid strokeDasharray="3 3" className="chart-grid" />
+        <Tooltip />
+        <Area type="monotone" dataKey="total" stroke="#8884d8" fillOpacity={1} fill="url(#total)" />
+        <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+      </AreaChart>
+    </ResponsiveContainer>
+  </div>
 );
 
 export default Chart;
