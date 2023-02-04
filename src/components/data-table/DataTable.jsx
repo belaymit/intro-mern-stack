da6bdff87fa0960d56869dcdc04b32b8131692ea
belaymit <2belamit@gmail.com>
2023-02-04 13:50:23 +0300

    added delete functionality

--- src/components/data-table/DataTable.jsx
+++ src/components/data-table/DataTable.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { Link } from 'react-router-dom';
 import { userColumns, userRows } from '../../common/dataTableSource';
 import '../../styles/dataTable.scss';
 
 const DataTable = () => {
+  const [data, setData] = useState(userRows);
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
   const actionColumn = [
     {
       field: 'action',
       headerName: 'Action',
       width: 200,
-      renderCell: () => (
+      renderCell: (params) => (
         <div className="cell-action">
-          <div className="view-button">View</div>
-          <div className="delete-button">Delete</div>
+          <Link to="single-user" style={{ textDecoration: 'none' }}>
+            <div className="view-button">View</div>
+          </Link>
+          <div className="delete-button" onClick={() => handleDelete(params.row.id)} role="presentation">Delete</div>
         </div>
       ),
     },
@@ -26,7 +32,7 @@ const DataTable = () => {
       </div>
       <DataGrid
         className="data-grid"
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         pageSize={5}
         rowsPerPageOptions={[5]}
