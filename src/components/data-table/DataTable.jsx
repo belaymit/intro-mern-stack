3de9c4126d27830c2a4d6a2c0b8192d270b23ebe
belaymit <2belamit@gmail.com>
2023-02-04 11:56:42 +0300

    added title and button

--- src/components/data-table/DataTable.jsx
+++ src/components/data-table/DataTable.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
 import { userColumns, userRows } from '../../common/dataTableSource';
 import '../../styles/dataTable.scss';
 
@@ -19,7 +20,12 @@ const DataTable = () => {
   ];
   return (
     <div className="data-table">
+      <div className="data-table-title">
+        Add New User
+        <Link to="/users/new" style={{ textDecoration: 'none' }} className="link">Add New User</Link>
+      </div>
       <DataGrid
+        className="data-grid"
         rows={userRows}
         columns={userColumns.concat(actionColumn)}
         pageSize={5}
