14f9720c22c92bc43b2077c4352194f4f9aa87ae
belaymit <2belamit@gmail.com>
2023-02-04 11:57:50 +0300

    added context and dispach toggle darkmode action

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
@@ -7,42 +7,50 @@ import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNone
 import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
 import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
 import './navbar.scss';
+import { DarkModeContext } from '../../context/darkModeContext';
 
-const Navbar = () => (
-  <div className="navbar">
-    <div className="wrapper">
-      <div className="search">
-        <input type="text" placeholder="Search..." />
-        <SearchOutlinedIcon className="icon" />
-      </div>
-      <div className="items">
-        <div className="item">
-          <LanguageOutlinedIcon className="icon" />
-          Languages
+const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext);
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search..." />
+          <SearchOutlinedIcon className="icon" />
         </div>
-        <div className="item">
-          <DarkModeOutlinedIcon className="icon" />
-        </div>
-        <div className="item">
-          <FullscreenExitOutlinedIcon className="icon" />
-        </div>
-        <div className="item">
-          <NotificationsNoneOutlinedIcon className="icon" />
-          <div className="counter">2</div>
-        </div>
-        <div className="item">
-          <ChatBubbleOutlineOutlinedIcon className="icon" />
-          <div className="counter">5</div>
-        </div>
-        <div className="item">
-          <ListOutlinedIcon className="icon" />
-        </div>
-        <div className="item">
-          <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" alt="user-avatar" className="avatar" />
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon" />
+            Languages
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon
+              className="icon"
+              onClick={() => dispatch({ type: 'TOGGLE' })}
+              style={{ cursor: 'pointer' }}
+            />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">5</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600" alt="user-avatar" className="avatar" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
